6e01456b52789898c0885e9127421da9371ecfec
Miroslav Kravec <kravec.miroslav@gmail.com>
2017-03-25 17:19:58 +0100

    [refactoring] moved all scripts under script directory

--- scripts/clean_build_test.sh
+++ scripts/clean_build_test.sh
@@ -7,5 +7,5 @@ cmake .. || exit 1
 make || exit 1
 
 cd .. || exit 1
-bash tests/test_lexer.sh || exit 1
-bash tests/test_parser.sh || exit 1
+bash scripts/tests/test_lexer.sh || exit 1
+bash scripts/tests/test_parser.sh || exit 1
